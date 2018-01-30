eaa979fd94c6686f25562e439cca116efc2ece04
Rifqi Fardi <rifqi.fardi@airfrov.com>
2018-01-30 15:17:02 +0700

    - make public utm_source

--- sharebottomsheetdialog/src/main/java/com/jboxx/sharebottomsheetdialog/UTMConstants.java
+++ sharebottomsheetdialog/src/main/java/com/jboxx/sharebottomsheetdialog/UTMConstants.java
@@ -20,11 +20,12 @@ public class UTMConstants {
     })
     public @interface UTM {}
 
-    public static final String UTM_CAMPAIGN = "utm_campaign";
+    public static  final  String UTM_CAMPAIGN  = "utm_campaign";
     public static final String UTM_CONTENT = "utm_content";
     public static final String UTM_MEDIUM = "utm_medium";
     public static final String UTM_TERM = "utm_term";
-    protected static final String UTM_SOURCE = "utm_source";
+    public static final String UTM_SOURCE = "utm_source";
+
 
     @Retention(RetentionPolicy.SOURCE)
     @StringDef({
